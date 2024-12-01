1d9d052c76d6ff604c97266cb425ddf5fd9712d6
Kazim Ali <kazim.ali@europace.de>
2024-12-01 14:26:07 +0100

    chore: add newsId field, disable cors

--- src/main/java/com/news/beacon/entity/NewsApiResponse.java
+++ src/main/java/com/news/beacon/entity/NewsApiResponse.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 @Setter
 @Getter
@@ -19,6 +20,7 @@ public class NewsApiResponse {
                 .filter(Objects::nonNull)  // Ensure the article is not null
                 .map(article -> {
                     return Map.of(
+                            "newsId", UUID.randomUUID().toString(),
                             "author", article.getAuthor() != null ? article.getAuthor() : "Unknown Author",
                             "title", article.getTitle() != null ? article.getTitle() : "Untitled",
                             "description", article.getDescription() != null ? article.getDescription() : "No Description",
